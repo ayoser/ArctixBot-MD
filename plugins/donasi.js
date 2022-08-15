e0bbc13dc39b2b1e660061642bca8111cf636d77
ayoser <106051450+ayoser@users.noreply.github.com>
2022-08-15 19:54:17 +0530

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,5 +1,5 @@
-let handler =  m => m.reply(` ❰𝐀𝐫𝐜𝐭𝐢𝐱 × 𝐀𝐫𝐜𝐞𝐮𝐬 𝐢𝐧𝐜.❱
-https://chat.whatsapp.com/Kb6msWfU8fP25KcG3M4Xtw
+let handler =  m => m.reply(` ❰🇬 🇦 🇷 🇫 🇮 🇪 🇱 🇩 ᵐᵈ❱
+ᗰᎩ ᑭᗴᖇᔑᝪᑎᗩし ᑌᔑᗴ ᝪᑎしᎩ
 `.trim()) // Add yourself if you want
 handler.help = ['donate']
 handler.tags = ['info']
