cb46577118435b87e572dad799fe357605a3e2d6
ayoser <106051450+ayoser@users.noreply.github.com>
2022-08-15 19:51:14 +0530

    Update youtube-ytv.js

--- plugins/youtube-ytv.js
+++ plugins/youtube-ytv.js
@@ -3,18 +3,18 @@ let handler = async (m, { conn, args, isPrems, isOwner }) => {
   if (!args || !args[0]) throw 'Wheres the link?'
   let { thumbnail, video, title } = await youtubedl(args[0])
       .catch(async () => await youtubedlv2(args[0]))
-  let link = await video['360p'].download()
+  let link = await video['1080p'].download()
   const isY = /y(es)/gi.test(args[1])
-  const limitedSize = (isPrems || isOwner ? 99 : 70) * 1024
-  let isLimit = limitedSize < video['360p'].fileSize
+  const limitedSize = (isPrems || isOwner ? 500 : 500) * 1024
+  let isLimit = limitedSize < video['1080p'].fileSize
   if (!isY) await conn.sendFile(m.chat, thumbnail, 'thumbnail.jpg', `
 *🔮𝗧𝗜𝗧𝗟𝗘:* ${title}
-*🔮𝗙𝗜𝗟𝗘𝗦𝗜𝗭𝗘:* ${video['360p'].fileSizeH}
+*🔮𝗙𝗜𝗟𝗘𝗦𝗜𝗭𝗘:* ${video['1080p'].fileSizeH}
 *${isLimit ? 'Uasge ' : ''}Link:* ${link}
 `.trim(), m)
 if (!isLimit) await conn.sendFile(m.chat, link, title + '.mp3', `
 *🔮𝗧𝗜𝗧𝗟𝗘:* ${title}
-*🔮𝗙𝗜𝗟𝗘𝗦𝗜𝗭𝗘:* ${video['360p'].fileSizeH}
+*🔮𝗙𝗜𝗟𝗘𝗦𝗜𝗭𝗘:* ${video['1080p'].fileSizeH}
 `.trim(), m, null, {
   asDocument: 0
 })
